122f83941bf931d9e8d45b36e41e4d072e3f0ba1
ddouglasRUI <34725801+ddouglasRUI@users.noreply.github.com>
2017-12-29 09:13:27 -0800

    Initial commit

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FAQ()
+        {
+            ViewBag.Message = "Frequently Asked Questions.";
+
+            return View();
+        }
+
+        public ActionResult ApproveRequests()
+        {
+            ViewBag.Message = "Approve Requests";
+
+            return View();
+        }
+    }
+}
